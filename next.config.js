76cce854976e87d3cdf2bb2088ed181e68192ead
blurk <nsinh6745@gmail.com>
2022-10-07 21:46:04 +0700

    chore: update config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['127.0.0.1', '66fb-222-252-30-231.ap.ngrok.io'],
+    domains: ['127.0.0.1', process.env.DOMAIN],
   },
 };
 
