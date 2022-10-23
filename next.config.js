89e111598b53b83b9125b1b72c9f092dba421b2a
blurk <nsinh6745@gmail.com>
2022-10-23 23:51:32 +0700

    feat: vercel config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   images: {
     domains: [process.env.NODE_ENV === 'development' ? '127.0.0.1' : process.env.DOMAIN],
   },
+  staticPageGenerationTimeout: 60, // seconds
 };
 
 module.exports = withPlaiceholder(nextConfig);
